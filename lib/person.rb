c297a90d197fb72382ec3095111b6d2386e8f450
Joe McPartland <joemcpartland@gmail.com>
2022-03-31 11:23:37 -0600

    commit tree 166d444831cdd183bef5997f88004caaea49a2ce

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,7 @@
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
 end
